1d77f13b9b1ef989396dde4be554a49c623c7d7f
andrea morales <amoralesp88@gmail.com>
2021-02-21 00:33:39 -0600

    working on passport config

---
+++ config/passport.config.js
@@ -0,0 +1,118 @@
+const passport = require('passport');
+const mongoose = require('mongoose')
+const LocalStrategy = require('passport-local').Strategy;
+const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+const User = require('../models/User.model')
+const FacebookStrategy = require('passport-facebook').Strategy;
+
+passport.serializeUser((user, next) => {
+    next(null, user.id);
+});
+
+passport.deserializeUser((id, next) => {
+    User.findById(id)
+        .then(user => next(null, user))
+        .catch(next);
+});
+
+passport.use('local-auth', new LocalStrategy({
+    usernameField: 'email',
+    passwordField: 'password'
+}, (email, password, next) => {
+    console.log('use passport')
+    User.findOne({ email: email })
+        .then((user) => {
+            if (!user) {
+                next(null, false, { error: "The email address or password is incorrect" })
+            } else {
+                return user.checkPassword(password)
+                    .then(match => {
+                        if (match) {
+                            if (user.active) {
+                                next(null, user)
+                            } else {
+                                next(null, false, { error: "Check Your email to activate your account" })
+                            }
+                        } else {
+                            next(null, false, { error: "The email address or password is incorrects" })
+                        }
+                    })
+            }
+        })
+        .catch(next)
+}))
+
+passport.use('google-auth', new GoogleStrategy({
+    clientID: process.env.G_CLIENT_ID,
+    clientSecret: process.env.G_CLIENT_SECRET,
+    callbackURL: process.env.G_REDIRECT_URI || '/authenticate/google/cb'
+}, (accessToken, refreshToken, profile, next) => {
+    const googleID = profile.id
+    const email = profile.emails[0] ? profile.emails[0].value : undefined;
+
+    if (googleID && email) {
+        User.findOne({
+                $or: [
+                    { email: email },
+                    { 'social.google': googleID }
+                ]
+            })
+            .then(user => {
+                if (!user) {
+                    const newUserInstance = new User({
+                        email,
+                        password: 'Aa1' + mongoose.Types.ObjectId(),
+                        social: {
+                            google: googleID
+                        },
+                        active: true
+                    })
+
+                    return newUserInstance.save()
+                        .then(newUser => next(null, newUser))
+                } else {
+                    next(null, user)
+                }
+            })
+            .catch(next)
+    } else {
+        next(null, null, { error: 'Error conectando con Google OAuth' })
+    }
+}))
+passport.use('google-auth', new GoogleStrategy({
+    clientID: process.env.G_CLIENT_ID,
+    clientSecret: process.env.G_CLIENT_SECRET,
+    callbackURL: process.env.G_REDIRECT_URI || '/authenticate/google/cb'
+}, (accessToken, refreshToken, profile, next) => {
+    const googleID = profile.id
+    const email = profile.emails[0] ? profile.emails[0].value : undefined;
+
+    if (googleID && email) {
+        User.findOne({
+                $or: [
+                    { email: email },
+                    { 'social.google': googleID }
+                ]
+            })
+            .then(user => {
+                if (!user) {
+                    const newUserInstance = new User({
+                        email,
+                        password: 'Aa1' + mongoose.Types.ObjectId(),
+                        social: {
+                            google: googleID
+                        },
+                        active: true
+                    })
+
+                    return newUserInstance.save()
+                        .then(newUser => next(null, newUser))
+                } else {
+                    next(null, user)
+                }
+            })
+            .catch(next)
+    } else {
+        next(null, null, { error: 'Error conectando con Google OAuth' })
+    }
+}))
---
+++ models/User.model.js
@@ -0,0 +1,69 @@
+const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+const EMAIL_PATTERN = /^(([^<>()[\]\\.,;:\s@']+(\.[^<>()[\]\\.,;:\s@']+)*)|('.+'))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+const PASSWORD_PATTERN = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/;
+const SALT_ROUNDS = 11
+const {
+    v4: uuidv4
+} = require('uuid'); //es el paquete de npm para generar token
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: 'The username field is required',
+        unique: true,
+        trim: true
+    },
+
+    email: {
+        type: String,
+        required: 'The email field is required',
+        unique: true,
+        lowercase: true,
+        match: [EMAIL_PATTERN, 'The email is not valid'],
+        trim: true
+    },
+    password: {
+        type: String,
+        required: 'The password field is required',
+        unique: true,
+        match: [PASSWORD_PATTERN, "The password you entered doesn't meet minimum security requirements"]
+
+    },
+    active: {
+        type: Boolean,
+        default: false,
+    },
+    social: {
+        google: String,
+        facebook: String,
+    },
+    activationToken: {
+        type: String,
+        default: () => {
+            return (
+                uuidv4() // asi se llama al token
+            );
+        },
+    },
+})
+
+userSchema.methods.checkPassword = function(passwordToCheck) {
+    return bcrypt.compare(passwordToCheck, this.password);
+};
+
+userSchema.pre('save', function(next) {
+    //user es igual a this
+
+    if (this.isModified('password')) {
+        bcrypt.hash(this.password, SALT_ROUNDS)
+            .then(hash => {
+                this.password = hash
+                next()
+            })
+    } else {
+        next()
+    }
+})
+const User = mongoose.model('User', userSchema)
+module.exports = User;
