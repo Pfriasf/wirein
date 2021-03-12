a5e7ba439e61fdb309514eb3d3439c1060af773c
Pedro Frias <plfriasf@gmail.com>
2021-03-12 08:29:25 -0800

    db setings updated

--- config/db.config.js
+++ config/db.config.js
@@ -1,18 +1,20 @@
 const mongoose = require("mongoose");
 
 mongoose
-  .connect(process.env.MONGODB_URI || "mongodb://localhost:27017/wiredin", 
-  {
+  .connect(process.env.MONGODB_URI || "mongodb://localhost:27017/wiredin", {
+    useCreateIndex: true,
     useNewUrlParser: true,
-    useUnifiedTopology: true,
+    useFindAndModify: false,
   })
-  .then(() => console.log("Succesfully conected to the DB"))
-  .catch((e) => console.error("Error conectiong to the DB", e));
+  .then((x) =>
+    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
+  )
+  .catch((err) => console.error("Error connecting to mongo", err));
 
 process.on("SIGINT", () => {
   mongoose.connection
     .close()
-    .then(() => console.log("Succesfully disconnected from the DB"))
+    .then(() => console.log("Successfully disconnected from the DB"))
     .catch((e) => console.error("Error disconnecting from the DB", e))
     .finally(() => process.exit());
 });
