e0be27e27af696f75cfd671c369a9c5edca2081a
Pedro Frias <plfriasf@gmail.com>
2021-03-12 07:39:42 -0800

    test#2

--- config/db.config.js
+++ config/db.config.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose
-  .connect(process.env.MONGODB_URI || "mongodb://localhost:27017/wiredin", {
+  .connect(process.env.MONGODB_URI , {
     useCreateIndex: true,
     useNewUrlParser: true,
     useUnifiedTopology: true,
