e480037d7ef78c238a55faa8aa921ac79f40dcc7
andrea morales <amoralesp88@gmail.com>
2021-03-12 02:29:08 -0600

    almost done

--- public/javascripts/checkout.js
+++ public/javascripts/checkout.js
@@ -1,10 +1,5 @@
 const stripe = Stripe('pk_test_51ISwPwIxhydthNECsuV4WqyNN6XN5V4PBeaXAoNHho6YpRVZVoY3gCvXw0BHoyuRQY8wwhtn2biHPihy5KTIkhsA00hC7qeVJC');
 
-/*const checkoutBtn = document.getElementById('checkout-button')
-if (checkoutBtn) {
-  checkoutBtn.addEventListener('click', function(e) {           
-  })
-}*/
 
 const checkoutButton = (id) =>{
   axios.post(`/service/${id}/buy`)
