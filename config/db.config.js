345bf11ec59fca7b9dac385371c24491646f703f
andrea morales <amoralesp88@gmail.com>
2021-03-12 16:23:03 -0600

    solved problems

--- config/db.config.js
+++ config/db.config.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose
-  .connect("mongodb://localhost:27017/wiredin", {
+ .connect(process.env.MONGODB_URI || "mongodb://localhost:27017/wiredin", {
     useCreateIndex: true,
     useNewUrlParser: true,
     useFindAndModify: false,
