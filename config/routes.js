40af83e6d9aa2dfdd4c4f3b623e4d57b9360910f
andrea morales <amoralesp88@gmail.com>
2021-03-12 18:06:19 -0600

    about almost done

--- config/routes.js
+++ config/routes.js
@@ -70,7 +70,7 @@ router.post("/service/:id/buy", secure.isAuthenticated, serviceController.buy)
 router.get("/service/:serviceId/like", secure.isAuthenticated, usersController.like);
 
 router.get("/test", function (req, res, next) {
-    res.render("about");
+    res.render("partials/about");
 });
 router.get("/contact", function (req, res, next) {
     res.render("contact");
